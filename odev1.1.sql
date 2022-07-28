1c276ebc0823bad2300505b804926adc3d443c88
Esma Şen <96180440+faradaystorm@users.noreply.github.com>
2022-07-28 18:34:46 +0300

    Create odev1.1.sql

---
+++ odev1.1.sql
@@ -0,0 +1,4 @@
+--city tablosu ile country tablosunda bulunan şehir (city) ve ülke (country) isimlerini birlikte görebileceğimiz INNER JOIN sorgusunu yazınız.
+SELECT city, country
+FROM country
+INNER JOIN ON city.country_id = country.country_id;
